2c1485493dbbd375b418dbb096b3aa5da7b633fb
kojurkin <kajukinia@stud.omsu.ru>
2022-03-15 23:39:30 +0600

    -

--- main.cpp
+++ main.cpp
@@ -3,35 +3,23 @@
 
 int main()
 {
-    double** array;
-    int  height, width;
-    std::ifstream inputfile ("output.txt");
-    inputfile >> width;
-    inputfile >> height;
-    array = new double*[height];
-    for (int i = 0; i < height; i++)
+    int  size;
+    std::ifstream inputfile ("input.txt");
+    inputfile >> size;
+    double* array = new double[size];
+
+    for(int i = 0; i < size; i++)
     {
-        array[i] = new double[width];
-    }
-    for (int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
+        if(inputfile.peek() == EOF)
         {
-            if(inputfile.peek() == EOF)
-            {
-                std::cout << "ERROR: not enough elements in file";
-                return 1;
-            }
-            inputfile >> array[i][j];
+            std::cout << "ERROR: not enough elements in file";
+            return 1;
         }
+        inputfile >> array[i];
     }
-    for(int i = 0; i < height; i++)
+    for(int i = 0; i < size; i++)
     {
-        for(int j = 0; j < width; j++)
-        {
-            std::cout << array[i][j] << " ";
-        }
-        std::cout << std::endl;
+        std::cout << array[i] << " ";
     }
     inputfile.close();
     return 0;
